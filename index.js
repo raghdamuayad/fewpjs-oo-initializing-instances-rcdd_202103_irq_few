0d5498724c98bef2f47452f4190d554ebc1b279a
Raghda  Muayad <therose000777@gmail.com>
2021-04-27 15:12:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your code here
+function  Breakfast(food, drink) {
+  this.food=food;
+  this.drink = drink;
+  this.sayHello = function() {
+    console.log(`${this.year},${this.drink}`);
+  };
+}
+  let bfast = new Breakfast('eggs', 'juice')
+  bfast.Breakfast();
+
+
+
+
+  function Lunch (salad,soup,drink) {
+    this.salad=salad;
+    this.soup=soup;
+    this.drink = drink;
+    this.sayHello = function() {
+      console.log(`${this.salad},${this.soup},${this.drink}`);
+    };
+  }
+  let newlunch = new Lunch('side salad','broccoli cheddar soup','iced tea')
+    newlunch.Lunch();
+
+
+
+
+
+
+      function Dinner (salad,soup,entree,dessert) {
+
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = dessert;
+        this.sayHello = function() {
+          console.log(`${this.soup},${this.entree},${this._dessert}`);
+        };
+      }
+    let newdinner = new Dinner('balsamic salad', 'consomme', 'filet mignon', 'cheesecake')
+        newdinner.Dinner();
